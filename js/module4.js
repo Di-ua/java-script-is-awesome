b08c15526804c2b2b45d6b78cc2eb329beab96c0
Diana Kuzmina <diana.kuzmina@users.noreply.github.com>
2020-11-23 09:08:26 +0200

    Add 4th module

---
+++ js/module4.js
@@ -0,0 +1,36 @@
+// const printMessage = function (message) {
+//   console.log(message);
+// };
+
+// const higherOrderFunction = function (callback) {
+//   const string = "HOCs are awesome";
+//   callback(string);
+// };
+
+// higherOrderFunction(printMessage);
+
+// const createCounter = function () {
+//   /*
+//    * Локальная переменная privateValue доступна только в замыкании.
+//    * Получить к ней доступ во внешнем коде невозможно.
+//    */
+//   let privateValue = 0;
+
+//   const increment = function () {
+//     privateValue += 1;
+//     console.log(privateValue);
+//   };
+
+//   return {
+//     increment,
+//   };
+// };
+
+// const counterA = createCounter();
+// counterA.increment(); // 1
+// counterA.increment(); // 2
+
+// const counterB = createCounter();
+// counterB.increment(); // 1
+// counterB.increment(); // 2
+// counterB.increment(); // 3
